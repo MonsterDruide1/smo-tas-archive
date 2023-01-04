0f6284f7bc722a73b4e89029ede2aac8d1fbb801
MonsterDruide1 <5958456@gmail.com>
2023-01-04 23:09:09 +0100

    SearchBar: Improve mobile flow of tags

--- lib/components/AuthorTag.tsx
+++ lib/components/AuthorTag.tsx
@@ -7,7 +7,7 @@ export default function AuthorTag(props: { author: AuthorEntry }) {
     const element = (
         <Fragment>
             <img src={props.author.icon} alt={`Profile-icon of ${props.author.name}`} className={styles.authorTagImg}/>
-            <p>{props.author.name}</p>
+            <p className={styles.authorTagText}>{props.author.name}</p>
         </Fragment>
     );
     return <Tag content={element} />
