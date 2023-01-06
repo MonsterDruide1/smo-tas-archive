d1002cf0b6446fb3a77b422b3baa51cb3b23812f
MonsterDruide1 <5958456@gmail.com>
2023-01-06 12:25:49 +0100

    AuthorTag: Use NextJS Image

--- lib/components/AuthorTag.tsx
+++ lib/components/AuthorTag.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Image from "next/image";
 import styles from "../../styles/Tag.module.css"
 import AuthorEntry from "../data/AuthorEntry";
 import Tag from './Tag'
@@ -6,7 +7,9 @@ import Tag from './Tag'
 export default function AuthorTag(props: { author: AuthorEntry }) {
     const element = (
         <Fragment>
-            <img src={props.author.icon} alt={`Profile-icon of ${props.author.name}`} className={styles.authorTagImg}/>
+            <div className={styles.authorTagImgWrapper}>
+                <Image fill src={props.author.icon} alt={`Profile-icon of ${props.author.name}`} className={styles.authorTagImg}/>
+            </div>
             <p className={styles.authorTagText}>{props.author.name}</p>
         </Fragment>
     );
