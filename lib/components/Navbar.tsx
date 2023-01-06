00128f7f4fa3717cd33e4c6fc4543d04cb14c13b
MonsterDruide1 <5958456@gmail.com>
2023-01-06 15:21:32 +0100

    Navbar: Logo as NextJS Image + selfhosted

--- lib/components/Navbar.tsx
+++ lib/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import styles from "../../styles/Navbar.module.css"
 
 export interface Props {
@@ -10,7 +11,7 @@ export default function Navbar(props: Props) {
     return <div className={styles.navbar}>
         <Link href="/">
             <div style={{width: "100%", height: "100%"}} onClick={!!props.onLogoClick ? props.onLogoClick : undefined}>
-                <img src={"https://cdn.discordapp.com/icons/829256569354321951/7a864c3a4114c5632efb901196a19afa.webp?size=96"} alt="Logo" className={styles.logo}/>
+                <Image width={40} height={40} src={"/smo/img/logo.png"} alt="Logo" className={styles.logo}/>
             </div>
         </Link>
         <p className={styles.current}>{props.current}</p>
