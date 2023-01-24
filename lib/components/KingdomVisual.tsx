9745daad7617c3721209b8a7eae1f63ec280fcb3
MonsterDruide1 <5958456@gmail.com>
2023-01-25 00:06:46 +0100

    Import Kindom thumbnails

--- lib/components/KingdomVisual.tsx
+++ lib/components/KingdomVisual.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames"
 import styles from "../../styles/KingdomVisual.module.css"
 import { KingdomType, getNiceName, getIcon } from "../data/Kingdom"
+import Image from "next/image"
 
 export interface Props {
     kingdom: KingdomType,
@@ -12,7 +13,9 @@ export default function Kingdom(props: Props) {
     var alt_text = `Icon of ${props.kingdom}`
     return <span className={classNames(styles.item, props.className)}>
         <span className={styles.inner_item} style={!!props.onClick ? {zIndex: 2, cursor: "pointer"} : {}} onClick={!!props.onClick ? () => props.onClick?.(props.kingdom) : undefined}>
-            <img className={styles.thumbnail} src={getIcon(props.kingdom)} alt={alt_text} />
+            <div className={styles.thumbnail_wrapper}>
+                <Image fill className={styles.thumbnail} src={getIcon(props.kingdom)} alt={alt_text} />
+            </div>
             <p className={styles.name}>{getNiceName(props.kingdom)}</p>
         </span>
     </span>
--- lib/data/Kingdom.tsx
+++ lib/data/Kingdom.tsx
@@ -20,43 +20,47 @@ export enum KingdomType {
 }
 
 export function getIcon(kingdom: KingdomType): string {
+    return `/smo/img/kingdoms/${getIconName(kingdom)}`;
+}
+
+function getIconName(kingdom: KingdomType): string {
     switch (kingdom as KingdomType) {
         case KingdomType.CAP:
-            return "https://mario.wiki.gallery/images/thumb/c/c9/SMO_Art_-_Cap_Kingdom.jpg/250px-SMO_Art_-_Cap_Kingdom.jpg";
+            return "Cap_Kingdom.jpg";
         case KingdomType.CASCADE:
-            return "https://mario.wiki.gallery/images/thumb/a/aa/SMO_Art_-_Cascade_Kingdom.jpg/250px-SMO_Art_-_Cascade_Kingdom.jpg";
+            return "Cascade_Kingdom.jpg";
         case KingdomType.SAND:
-            return "https://mario.wiki.gallery/images/thumb/a/ae/SMO_Art_-_Sand_Kingdom.jpg/250px-SMO_Art_-_Sand_Kingdom.jpg";
+            return "Sand_Kingdom.jpg";
         case KingdomType.LAKE:
-            return "https://mario.wiki.gallery/images/thumb/3/3e/SMO_Art_-_Lake_Kingdom.jpg/250px-SMO_Art_-_Lake_Kingdom.jpg";
+            return "Lake_Kingdom.jpg";
         case KingdomType.WOODED:
-            return "https://mario.wiki.gallery/images/thumb/f/fb/SMO_Art_-_Wooded_Kingdom.jpg/250px-SMO_Art_-_Wooded_Kingdom.jpg";
+            return "Wooded_Kingdom.jpg";
         case KingdomType.CLOUD:
-            return "https://mario.wiki.gallery/images/thumb/8/8a/SMO_CLOUD.jpg/250px-SMO_CLOUD.jpg";
+            return "Cloud_Kingdom.jpg";
         case KingdomType.LOST:
-            return "https://mario.wiki.gallery/images/thumb/2/2b/SMO_LOST.jpg/250px-SMO_LOST.jpg";
+            return "Lost_Kingdom.jpg";
         case KingdomType.METRO:
-            return "https://mario.wiki.gallery/images/thumb/b/ba/SMO_Art_-_Metro_Kingdom.jpg/250px-SMO_Art_-_Metro_Kingdom.jpg";
+            return "Metro_Kingdom.jpg";
         case KingdomType.SNOW:
-            return "https://mario.wiki.gallery/images/thumb/3/31/SMO_Art_-_Snow_Kingdom.png/250px-SMO_Art_-_Snow_Kingdom.png";
+            return "Snow_Kingdom.png";
         case KingdomType.SEASIDE:
-            return "https://mario.wiki.gallery/images/thumb/c/cf/SMO_Seaside_Kingdom.jpg/250px-SMO_Seaside_Kingdom.jpg";
+            return "Seaside_Kingdom.jpg";
         case KingdomType.LUNCHEON:
-            return "https://mario.wiki.gallery/images/thumb/d/d3/SMO_Art_-_Luncheon_Kingdom.jpg/250px-SMO_Art_-_Luncheon_Kingdom.jpg";
+            return "Luncheon_Kingdom.jpg";
         case KingdomType.RUINED:
-            return "https://mario.wiki.gallery/images/thumb/a/a5/SMO_RUIN.jpg/250px-SMO_RUIN.jpg";
+            return "Ruined_Kingdom.jpg";
         case KingdomType.BOWSERS:
-            return "https://mario.wiki.gallery/images/thumb/2/25/SMO_BOWSER.jpg/250px-SMO_BOWSER.jpg";
+            return "Bowsers_Kingdom.jpg";
         case KingdomType.MOON:
-            return "https://mario.wiki.gallery/images/thumb/c/c8/SMO_MOON.jpg/250px-SMO_MOON.jpg";
+            return "Moon_Kingdom.jpg";
         case KingdomType.MUSHROOM:
-            return "https://mario.wiki.gallery/images/thumb/5/59/SMO_-_Mushroom_Kingdom.jpg/250px-SMO_-_Mushroom_Kingdom.jpg";
+            return "Mushroom_Kingdom.jpg";
         case KingdomType.DARK:
-            return "https://mario.wiki.gallery/images/thumb/7/7a/DarkSide_SMO.png/250px-DarkSide_SMO.png";
+            return "Dark_Side.png";
         case KingdomType.DARKER:
-            return "https://mario.wiki.gallery/images/thumb/3/36/SMO_Shot_-_Darker_Side.jpg/250px-SMO_Shot_-_Darker_Side.jpg";
+            return "Darker_Side.jpg";
         default:
-            return "https://styles.redditmedia.com/t5_3ihls/styles/communityIcon_vfbddda2r9011.png"
+            return "Unknown.webp"
     }
 }
 
